a78b689b5f725e42a3914dbe36f1a73aaf23ef92
Oleh Busko <busko.oleh@gmail.com>
2020-12-20 10:44:54 +0000

    Finished writing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,65 @@ import { getPeople } from "./api/people";
 import { getPlanets } from './api/planets';
 import { people, planets, planetsMap } from './data/testData';
 import { MainProvider, reducerInit } from './contexts/mainContext';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, cleanup } from '@testing-library/react';
 
 jest.mock("./api/people");
 jest.mock("./api/planets");
 
 describe('App testing', () => {
+  afterEach(cleanup);
+
+  beforeEach(() => {
+    getPeople.mockImplementation(({ search, nextUrl }: ApiParams) => {
+      const maxPerPage = 1;
+      if (search) {
+        const data = people.filter((person: Person) => (
+          person.name.toLowerCase().includes(search.toLowerCase())
+        ));
+        conosle.log(data.length)
+        return {
+          data: {
+            previous: null,
+            results: [data[0]],
+            next: data.length > maxPerPage ? '/nextpage' : null
+          }
+        }
+      } else if (nextUrl === '/nextpage') {
+        return {
+          data: {
+            next: null,
+            previous: '/prevpage',
+            results: [people[1]]
+          }
+        }
+      } else if (nextUrl === '/prevpage') {
+        return {
+          data: {
+            previous: null,
+            next: '/nextpage',
+            results: [people[0]],
+          }
+        }
+      } else {
+        return {
+          data: {
+            previous: null,
+            next: '/nextpage',
+            results: [people[0]]
+          }
+        }
+      }
+    });
+
+    getPlanets.mockResolvedValueOnce({
+      data: {
+        next: null,
+        previous: null,
+        results: planets
+      }
+    });
+  });
+
   const contextRender = (app: ReactNode) => {
     return render(
       <MainProvider init={reducerInit}>
@@ -18,7 +71,7 @@ describe('App testing', () => {
     );
   }
 
-  it('Must render initial page', () => {
+  it('Must render initial state of App component', () => {
     render(
       <App />
     );
@@ -31,26 +84,10 @@ describe('App testing', () => {
   });
 
 
-  it('Must render initial page and load mock data', async () => {
+  it('Must render App component and load mock data', async () => {
     const { container, debug } = contextRender(
       <App />
     );
-
-    getPeople.mockResolvedValueOnce({
-      data: {
-        next: null,
-        previous: null,
-        results: people
-      }
-    });
-
-    getPlanets.mockResolvedValueOnce({
-      data: {
-        next: null,
-        previous: null,
-        results: planets
-      }
-    });
 
     const loadElement = screen.getByTestId('button-loaddata');
     fireEvent.click(loadElement);
@@ -69,12 +106,92 @@ describe('App testing', () => {
     await waitFor(() => {
       expect(screen.getByTestId('population-results')).toBeInTheDocument();
     });
-    expect(container.querySelectorAll('.personRow').length).toBe(2);
+
+    expect(container.querySelectorAll('.personRow').length).toBe(1);
 
     const nextElement = screen.getByTestId('button-next');
     const prevElement = screen.getByTestId('button-previous');
 
-    expect(nextElement).toBeDisabled();
+
+    expect(nextElement).not.toBeDisabled();
     expect(prevElement).toBeDisabled();
+  });
+
+  it('Must render App component and navigate between pages', async () => {
+    const { container, debug } = contextRender(
+      <App />
+    );
+
+    const loadElement = screen.getByTestId('button-loaddata');
+    fireEvent.click(loadElement);
+
+    expect(getPeople).toHaveBeenCalledTimes(1);
+    expect(getPlanets).toHaveBeenCalledTimes(1);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('population-results')).toBeInTheDocument();
+    });
+
+    expect(container.querySelectorAll('.personRow').length).toBe(1);
+
+    const nextElement = screen.getByTestId('button-next');
+    const prevElement = screen.getByTestId('button-previous');
+
+    expect(prevElement).toBeDisabled();
+
+    fireEvent.click(nextElement);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('population-results')).toBeInTheDocument();
+    });
+
+    expect(getPeople).toHaveBeenCalledTimes(2);
+
+    expect(prevElement).not.toBeDisabled();
+    expect(nextElement).toBeDisabled();
+
+    fireEvent.click(prevElement);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('population-results')).toBeInTheDocument();
+    });
+
+    expect(getPeople).toHaveBeenCalledTimes(3);
+
+    expect(prevElement).toBeDisabled();
+    expect(nextElement).not.toBeDisabled();
+  });
+
+  it('Must render App component and search for data', async () => {
+    const { container, debug } = contextRender(
+      <App />
+    );
+
+    const loadElement = screen.getByTestId('button-loaddata');
+    fireEvent.click(loadElement);
+
+    expect(getPeople).toHaveBeenCalledTimes(1);
+    expect(getPlanets).toHaveBeenCalledTimes(1);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('population-results')).toBeInTheDocument();
+    });
+
+    const searchElement = screen.getByTestId('search-input');
+    const searchButtonElement = screen.getByTestId('search-button');
+    fireEvent.change(searchElement, { target: { value: 'li' } });
+    fireEvent.click(searchButtonElement);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('population-results')).toBeInTheDocument();
+    });
+
+    expect(container.querySelectorAll('.personRow').length).toBe(1);
+
+    const nextElement = screen.getByTestId('button-next');
+    const prevElement = screen.getByTestId('button-previous');
+
+    expect(prevElement).toBeDisabled();
+    expect(nextElement).toBeDisabled();
   });
 });
